8b4e7cf98197565d998dd1bb74be02d7f39fe458
Vitalii Yartym <vyartim@gmail.com>
2020-12-28 19:53:08 +0200

    Added help materials

---
+++ ComputerGraphics/ViewModels/HelpAffineViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+using ComputerGraphics.Commands;
+
+namespace ComputerGraphics.ViewModels
+{
+    public class HelpAffineViewModel : BaseViewModel
+    {
+        public ICommand NavigateBack => new RelayCommand((obj) =>
+        {
+            MainWindow mw = (MainWindow)App.Current.MainWindow;
+            mw.MainFrame.Navigate(new Views.FractalView());
+        });
+    }
+}
